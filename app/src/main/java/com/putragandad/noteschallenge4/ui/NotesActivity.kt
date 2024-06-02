36760bdeb6c3b14a83e8b887b598f12da4008660
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2024-06-03 04:35:02 +0700

    More simpler approach with themes.xml

--- app/src/main/java/com/putragandad/noteschallenge4/ui/NotesActivity.kt
+++ app/src/main/java/com/putragandad/noteschallenge4/ui/NotesActivity.kt
@@ -11,21 +11,5 @@ class NotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            val window: Window = window
-            window.navigationBarColor = resources.getColor(R.color.white)
-            window.statusBarColor = resources.getColor(R.color.white)
-
-            // Check if the device supports light status bar and navigation bar icons
-            var flags = window.decorView.systemUiVisibility
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                flags = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR // Light status bar icons
-                flags = flags or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR // Light navigation bar icons
-            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                flags = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR // Light status bar icons
-            }
-            window.decorView.systemUiVisibility = flags
-        }
     }
 }
