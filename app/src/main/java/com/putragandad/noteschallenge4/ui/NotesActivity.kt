889a9a39bf32be3bad9b9e49f59d67a8bcc95f62
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2024-06-03 04:25:43 +0700

    Set theme to make device theme match app colors

--- app/src/main/java/com/putragandad/noteschallenge4/ui/NotesActivity.kt
+++ app/src/main/java/com/putragandad/noteschallenge4/ui/NotesActivity.kt
@@ -1,12 +1,31 @@
 package com.putragandad.noteschallenge4.ui
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
 import com.putragandad.noteschallenge4.R
 
 class NotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window: Window = window
+            window.navigationBarColor = resources.getColor(R.color.white)
+            window.statusBarColor = resources.getColor(R.color.white)
+
+            // Check if the device supports light status bar and navigation bar icons
+            var flags = window.decorView.systemUiVisibility
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                flags = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR // Light status bar icons
+                flags = flags or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR // Light navigation bar icons
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                flags = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR // Light status bar icons
+            }
+            window.decorView.systemUiVisibility = flags
+        }
     }
 }
